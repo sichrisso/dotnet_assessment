b43a4c9b1c4a0f6eaabc3577c02e6c873d3d4b3e
Christina Hailu <christinahailu@Christinas-MacBook-Air.local>
2023-10-13 22:45:51 +0300

    added validation

--- WebApi/Models/Cinema.cs
+++ WebApi/Models/Cinema.cs
@@ -3,6 +3,7 @@ namespace WebApi.Models
 {
     public class Cinema
     {
+        
         public int Id {get; set; }
         public string Name { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
